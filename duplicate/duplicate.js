431c3f351c4c68974323df8502993ee2fcc3c2f8
nelsonelaye <nelsonelaye@gmail.com>
2022-03-02 11:03:50 +0100

    Completed the duplicate algorithm

--- duplicate/duplicate.js
+++ duplicate/duplicate.js
@@ -1,15 +1,23 @@
-const elements = ["a", "b", 1, 6, "a", "6", "c"]
+//pick each individual element
+//find if there exist more than one of that element
+//if true, remove one else 
+
+const elements = ["a", "b", 1, 6, "a", 6, "c"]
 
 const findDuplicate = () => {
 
     for(el of elements) {
-        let elIndex = elements.indexOf(el)
-        for(let i=0; i<elements.length; i++) {
-            
-        }
+       let dup = elements.filter((e) => e === el)
+        console.log(dup)
 
+        if(dup.length > 1) {
+            let newEl = dup[0]
+            let dupIndex = elements.lastIndexOf(newEl)
+            elements.splice(dupIndex, 1)
+        }
     }
-   
+
+    console.log(elements)
 }
 
 findDuplicate()
